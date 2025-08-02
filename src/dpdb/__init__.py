1c5535b578905f2947e95ff31288ca11c4c9837f
starreeze <starreeze@foxmail.com>
2025-08-02 11:10:22 +0800

    one process dump support

--- src/dpdb/__init__.py
+++ src/dpdb/__init__.py
@@ -1,3 +1,5 @@
+import os
+import warnings
 from datetime import datetime
 
 from .core_dump import (  # noqa: F401
@@ -7,11 +9,18 @@ from .core_dump import (  # noqa: F401
     uninstall_global_handler,
 )
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
-def dump(path: str | None = None):
+def dump(path: str | None = None, dump_one_process_only: bool = True):
     d = CoreDumpGenerator.create_from_current_stack()
     if path is None:
-        path = f"core_dump_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pkl"
+        path = f"./core_dump_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pkl"
+    if dump_one_process_only:
+        lock_file = os.path.join(os.path.dirname(path), "dpdb.lock")
+        if os.path.exists(lock_file):
+            warnings.warn(f"Another process is already dumping core dump. Skip process {os.getpid()}")
+            return
+        open(lock_file, "w").close()
     save_core_dump(d, path)
+    os.remove(lock_file)
